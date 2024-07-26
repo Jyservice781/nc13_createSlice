982c8739af48e62b64895f6502330bb404a48c90
BITCAMP <gimjuyeong512@gmail.com>
2024-07-26 12:25:07 +0900

    commit 02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {addToDo} from "./redux/todoSlice";
+import {addToDo, deleteToDo, toggleToDo} from "./redux/todoSlice";
 
 // 참고 : https://velog.io/@hodoo/Redux-toolkit%EC%9C%BC%EB%A1%9C-To-do-list-%EB%A7%8C%EB%93%A4%EA%B8%B0
 
@@ -29,8 +29,10 @@ function App() {
         let currentDate = new Date()
         let dueDate = new Date(deadline)
         let timeDiff = dueDate - currentDate
+        timeDiff = timeDiff / 1000 / 60 / 60 / 24
+        console.log(timeDiff)
 
-        return timeDiff <= 0 ? 'red' : timeDiff <= 1 ? 'green' : 'blue'
+        return timeDiff <= 1 ? 'red' : timeDiff <= 2 ? 'green' : 'blue'
     }
 
     return (
@@ -47,6 +49,31 @@ function App() {
                        onChange={e => setDeadline(e.target.value)} required/>
                 <button type="submit">할 일 추가하기</button>
             </form>
+            <ul>
+                {todos.map(t => (
+                    <li
+                        key={t.id}
+                        style={{
+                            backgroundColor: getBackgroundColor(t.deadline),
+                            textDecoration: t.completed ? 'line-through' : 'none',
+                            listStyle: 'none',
+                        }}
+                    >
+                        <h1>{t.name}</h1>
+                        <h2>{t.detail}</h2>
+                        <h2>마감기한: {new Date(t.deadline).toLocaleDateString()}</h2>
+                        <h2>추가일: {new Date(t.created).toLocaleDateString()}</h2>
+                        <div>
+                            <button onClick={() => dispatch(toggleToDo(t.id))}>
+                                {t.completed ? '미완료' : '다했다'}
+                            </button>
+                            <button onClick={() => dispatch(deleteToDo(t.id))}>
+                                삭제하기
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,6 +1,20 @@
 import {createSlice} from '@reduxjs/toolkit'
 import {v4 as uuidv4} from 'uuid'
 
+// create Store 를 사용하지 않고 redux tool kit 을 사용할 예정
+// 기능별 정리
+// createSlice
+// 1. slice 를 생성: 슬라이스는 리듀서 로직과 액션을 함께 정의한다.
+// 2. 스토어 구성
+// 3. 리액트 앱에 스토어 제공
+// 4. 컴포넌트 상태 사용 및 액션 디스패치
+// 이 방식을 사용하게 되면
+// createSlice 를 사용하여 액션 생성자와 리듀서를 한 번에 정의할 수 있다ㅏ.
+// configureStore 가 자동으로 ReduxDevTools 확장을 설치한다.
+// 불변성을 위해 immer 라이브러리를 내부적으로 사용하므로, 상태를 직접 수정하는 것처럼 코드를 작성 할 수 있다.
+// 비동기 액션 처리가 용이하다.
+
+
 let initialState = {
     todos: []
 }
